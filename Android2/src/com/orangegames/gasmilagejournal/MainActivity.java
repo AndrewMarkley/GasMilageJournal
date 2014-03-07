cb19f2a51043d82d551af3fc972c9797d443d8f8
AndrewMarkley <andrew-tx@satx.rr.com>
2014-03-07 00:57:20 -0600

    Added options menu activities

--- Android2/src/com/orangegames/gasmilagejournal/MainActivity.java
+++ Android2/src/com/orangegames/gasmilagejournal/MainActivity.java
@@ -2,6 +2,7 @@ package com.orangegames.gasmilagejournal;
 
 import java.util.Locale;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -9,7 +10,10 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
+import android.view.MenuItem;
 
+import com.orangegames.gasmilagejournal.activities.AboutActivity;
+import com.orangegames.gasmilagejournal.activities.ExportActivity;
 import com.orangegames.gasmilagejournal.fragments.CarsViewFragment;
 import com.orangegames.gasmilagejournal.fragments.FillUpViewFragment;
 import com.orangegames.gasmilagejournal.fragments.StatisticsViewFragment2;
@@ -39,6 +43,26 @@ public class MainActivity extends FragmentActivity
 	{
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	    case R.id.action_export:
+	    	Intent intent = new Intent(this, ExportActivity.class);
+            startActivity(intent);
+	        return true;
+	    case R.id.action_about:
+	    	intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+	        return true;
+	    case R.id.action_settings:
+	    	intent = new Intent(this, ExportActivity.class);
+            startActivity(intent);
+	        return true;
+	    default:
+	        return super.onOptionsItemSelected(item);
+	    }
 	}
 
 	/**
---
+++ Android2/src/com/orangegames/gasmilagejournal/activities/SettingsActivity.java
@@ -0,0 +1,18 @@
+package com.orangegames.gasmilagejournal.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.orangegames.gasmilagejournal.R;
+
+public class SettingsActivity extends Activity
+{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.options_settings);
+	}
+
+}
