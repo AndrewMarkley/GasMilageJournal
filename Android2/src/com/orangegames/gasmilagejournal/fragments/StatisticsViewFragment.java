a4eaefd47be937d2781a4bea9eae580b1ef67904
AndrewMarkley <andrew-tx@satx.rr.com>
2014-03-11 20:41:50 -0500

    added best and worst to statistics, removed milage per doller graph

--- Android2/src/com/orangegames/gasmilagejournal/fragments/StatisticsViewFragment.java
+++ Android2/src/com/orangegames/gasmilagejournal/fragments/StatisticsViewFragment.java
@@ -2,6 +2,7 @@ package com.orangegames.gasmilagejournal.fragments;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.achartengine.ChartFactory;
@@ -16,7 +17,6 @@ import org.achartengine.renderer.XYSeriesRenderer;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.Display;
@@ -30,11 +30,9 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.internal.di;
 import com.j256.ormlite.stmt.PreparedQuery;
 import com.j256.ormlite.stmt.QueryBuilder;
 import com.orangegames.gasmilagejournal.R;
@@ -55,39 +53,33 @@ public class StatisticsViewFragment extends Fragment
 	LinearLayout lPPG = null;
 	LinearLayout lMFC = null;
 	LinearLayout lCPM = null;
-	LinearLayout lMPD = null;
 	LinearLayout lMM = null;
 
 	GraphicalView gviewMPG = null;
 	GraphicalView gviewPricePerGallon = null;
 	GraphicalView gviewMonthlyFuelCosts = null;
 	GraphicalView gviewCostPerMile = null;
-	GraphicalView gviewMilagePerDollar = null;
 	GraphicalView gviewMonthlyMilage = null;
+
+	private View rootView = null;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
-		View rootView = inflater.inflate(R.layout.statistics_view_fragment, container, false);
+		rootView = inflater.inflate(R.layout.statistics_view_fragment, container, false);
 
 		carList = (Spinner) rootView.findViewById(R.id.statistics_view_fragment_car_spinner);
 		lMPG = (LinearLayout) rootView.findViewById(R.id.statistics_view_fragment_mpg_graph);
 		lPPG = (LinearLayout) rootView.findViewById(R.id.statistics_view_fragment_ppg_graph);
 		lMFC = (LinearLayout) rootView.findViewById(R.id.statistics_view_fragment_mfc_graph);
 		lCPM = (LinearLayout) rootView.findViewById(R.id.statistics_view_fragment_cpm_graph);
-		lMPD = (LinearLayout) rootView.findViewById(R.id.statistics_view_fragment_mpd_graph);
 		lMM = (LinearLayout) rootView.findViewById(R.id.statistics_view_fragment_mm_graph);
 
 		gviewMPG = getMPGGraph();
 		gviewPricePerGallon = getPPGGraph();
 		gviewMonthlyFuelCosts = getMFCGraph();
 		gviewCostPerMile = getCPMGraph();
-		gviewMilagePerDollar = getMPDGraph();
 		gviewMonthlyMilage = getMMGraph();
-
-		AdView adView = (AdView) rootView.findViewById(R.id.adView);
-		AdRequest adRequest = new AdRequest.Builder().addTestDevice("89C5255F42662D2FCFD03698061CF86D").build();
-		adView.loadAd(adRequest);
 
 		List<Car> cars = new ArrayList<Car>();
 		try {
@@ -189,16 +181,65 @@ public class StatisticsViewFragment extends Fragment
 
 	public void addAllGraphViews()
 	{
+		removeAllGraphViews();
+		
+		List<FillUp> fillups = null;
+		try {
+			fillups = getFillUpDatabaseHelper().getFillUpDao().queryForAll();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		View container = View.inflate(getActivity().getApplicationContext(), R.layout.statistics_view_high_container, (ViewGroup) rootView.findViewById(R.id.high));
+		View container2 = View.inflate(getActivity().getApplicationContext(), R.layout.statistics_view_high_container, (ViewGroup) rootView.findViewById(R.id.high));
+		View container3 = View.inflate(getActivity().getApplicationContext(), R.layout.statistics_view_high_container, (ViewGroup) rootView.findViewById(R.id.high));
+		View container4 = View.inflate(getActivity().getApplicationContext(), R.layout.statistics_view_high_container, (ViewGroup) rootView.findViewById(R.id.high));
+		View container5 = View.inflate(getActivity().getApplicationContext(), R.layout.statistics_view_high_container, (ViewGroup) rootView.findViewById(R.id.high));
+		
+		TextView h = (TextView) container.findViewById(R.id.statistics_view_high_text);
+		TextView l = (TextView) container.findViewById(R.id.statistics_view_low_text);
+
+		h.setText("Best: " + getBestMPGRecord(fillups));
+		l.setText("Worst: " + getWorstMPGRecord(fillups));
+		lMPG.addView(container);
+		
+		h = (TextView) container2.findViewById(R.id.statistics_view_high_text);
+		l = (TextView) container2.findViewById(R.id.statistics_view_low_text);
+		
+		h.setText("Best: " + getBestPPGRecord(fillups));
+		l.setText("Worst: " + getWorstPPGRecord(fillups));
+		lPPG.addView(container2);
+		
+		h = (TextView) container3.findViewById(R.id.statistics_view_high_text);
+		l = (TextView) container3.findViewById(R.id.statistics_view_low_text);
+		
+		h.setText("Best: " + getBestMFCRecord(fillups));
+		l.setText("Worst: " + getWorstMFCRecord(fillups));
+		lMFC.addView(container3);
+		
+		h = (TextView) container4.findViewById(R.id.statistics_view_high_text);
+		l = (TextView) container4.findViewById(R.id.statistics_view_low_text);
+		
+		h.setText("Best: " + getBestCPMRecord(fillups));
+		l.setText("Worst: " + getWorstMFCRecord(fillups));
+		lCPM.addView(container4);
+		
+		h = (TextView) container5.findViewById(R.id.statistics_view_high_text);
+		l = (TextView) container5.findViewById(R.id.statistics_view_low_text);
+		
+		h.setText("Best: " + getBestMMRecord(fillups));
+		l.setText("Worst: " + getWorstMMRecord(fillups));
+		lMM.addView(container5);		
+		
 		WindowManager wm = (WindowManager) getActivity().getBaseContext().getSystemService(Context.WINDOW_SERVICE);
 		Display display = wm.getDefaultDisplay();
-		int yPx = (int)( display.getHeight() * 0.7);
-		removeAllGraphViews();
+		int yPx = (int) ( display.getHeight() * 0.7 );
 		lMPG.addView(getMPGGraph(), new LayoutParams(LayoutParams.MATCH_PARENT, yPx));
 		lPPG.addView(getPPGGraph(), new LayoutParams(LayoutParams.MATCH_PARENT, yPx));
 		lMFC.addView(getMFCGraph(), new LayoutParams(LayoutParams.MATCH_PARENT, yPx));
 		lCPM.addView(getCPMGraph(), new LayoutParams(LayoutParams.MATCH_PARENT, yPx));
-		lMPD.addView(getMPDGraph(), new LayoutParams(LayoutParams.MATCH_PARENT, yPx));
 		lMM.addView(getMMGraph(), new LayoutParams(LayoutParams.MATCH_PARENT, yPx));
+
 	}
 
 	public void removeAllGraphViews()
@@ -207,7 +248,6 @@ public class StatisticsViewFragment extends Fragment
 		lPPG.removeAllViews();
 		lMFC.removeAllViews();
 		lCPM.removeAllViews();
-		lMPD.removeAllViews();
 		lMM.removeAllViews();
 	}
 
@@ -217,12 +257,11 @@ public class StatisticsViewFragment extends Fragment
 		gviewPricePerGallon.repaint();
 		gviewMonthlyFuelCosts.repaint();
 		gviewCostPerMile.repaint();
-		gviewMilagePerDollar.repaint();
 		gviewMonthlyMilage.repaint();
 
 	}
 
-	//LineChart
+	// LineChart
 	public GraphicalView getMPGGraph()
 	{
 		GraphicalView gview = null;
@@ -289,7 +328,7 @@ public class StatisticsViewFragment extends Fragment
 		return gview;
 	}
 
-	//Scatter Plot
+	// Scatter Plot
 	public GraphicalView getPPGGraph()
 	{
 		GraphicalView gview = null;
@@ -357,7 +396,7 @@ public class StatisticsViewFragment extends Fragment
 		return gview;
 	}
 
-	//Bar Graph
+	// Bar Graph
 	public GraphicalView getMFCGraph()
 	{
 		GraphicalView gview = null;
@@ -378,8 +417,7 @@ public class StatisticsViewFragment extends Fragment
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-			
-			
+
 			for ( FillUp fu : fillUps ) {
 				double pos = Double.parseDouble(fu.getDate().getMonth() + "." + fu.getDate().getYear());
 				series.add(pos, fu.getTotalCost());
@@ -425,7 +463,7 @@ public class StatisticsViewFragment extends Fragment
 		return gview;
 	}
 
-	//Scatter Plot
+	// Scatter Plot
 	public GraphicalView getCPMGraph()
 	{
 		GraphicalView gview = null;
@@ -492,77 +530,8 @@ public class StatisticsViewFragment extends Fragment
 		gview = ChartFactory.getScatterChartView(getActivity(), dataset, renderer);
 		return gview;
 	}
-	
-	//LineChart
-	public GraphicalView getMPDGraph()
-	{
-		GraphicalView gview = null;
 
-		// Create the dataset
-		XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
-		TimeSeries series = new TimeSeries("Date");
-
-		if ( carList.getSelectedItem() != null ) {
-			Car car = (Car) carList.getSelectedItem();
-			List<FillUp> fillUps = null;
-
-			try {
-				QueryBuilder<FillUp, Integer> queryBuilder = getFillUpDatabaseHelper().getFillUpDao().queryBuilder();
-				queryBuilder.where().eq(FillUp.COLUMN_CAR_ID, car.getId());
-
-				PreparedQuery<FillUp> preparedQuery = queryBuilder.prepare();
-				fillUps = getFillUpDatabaseHelper().getFillUpDao().query(preparedQuery);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-
-			int x = 0;
-			for ( FillUp fu : fillUps ) {
-				series.add(x++, fu.getMPG());
-
-			}
-		}
-
-		dataset.addSeries(series);
-
-		// Setup the renderers
-		XYSeriesRenderer r = new XYSeriesRenderer();
-		r.setColor(Color.YELLOW);
-		r.setPointStyle(PointStyle.DIAMOND);
-		r.setFillBelowLine(false);
-		r.setFillPoints(true);
-
-		XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
-		renderer.setAxisTitleTextSize(35);
-		renderer.setChartTitleTextSize(50);
-		renderer.setLabelsTextSize(35);
-		renderer.setPointSize(5f);
-		renderer.setZoomEnabled(false, false);
-		// top, left, bottom, right
-		renderer.setMargins(new int[] { 80, 80, 20, 0 });
-		renderer.setChartTitle("Milage per Dollar");
-		renderer.setXTitle("Date");
-		renderer.setYTitle("Milage");
-		renderer.setApplyBackgroundColor(false);
-		renderer.setRange(new double[] { 0, 6, 0, 40 });
-		renderer.setFitLegend(false);
-		renderer.setPanEnabled(false, false);
-		renderer.setZoomEnabled(false, false);
-		renderer.setAxesColor(Color.RED);
-		renderer.setShowGrid(true);
-		renderer.setZoomEnabled(false);
-		renderer.setInScroll(true);
-		renderer.addSeriesRenderer(r);
-		renderer.setXAxisMin(0);
-		renderer.setXAxisMax(dataset.getSeriesAt(0).getMaxX() + 0.5);
-		renderer.setYAxisMin(0);
-		renderer.setYAxisMax(dataset.getSeriesAt(0).getMaxY() + 0.5);
-
-		gview = ChartFactory.getLineChartView(getActivity(), dataset, renderer);
-		return gview;
-	}
-
-	//BarGraph
+	// BarGraph
 	public GraphicalView getMMGraph()
 	{
 		GraphicalView gview = null;
@@ -584,7 +553,7 @@ public class StatisticsViewFragment extends Fragment
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-			
+
 			for ( FillUp fu : fillUps ) {
 				series.add(fu.getDate().getMonth(), fu.getDistance());
 			}
@@ -629,4 +598,154 @@ public class StatisticsViewFragment extends Fragment
 		return gview;
 	}
 
+	public double getBestMPGRecord(List<FillUp> fillups)
+	{
+		double best = 0.0;
+		for ( FillUp fu : fillups ) {
+			if ( best < fu.getMPG() ) {
+				best = fu.getMPG();
+			}
+		}
+		return round(best);
+	}
+
+	public double getWorstMPGRecord(List<FillUp> fillups)
+	{
+		double best = Double.MAX_VALUE;
+		for ( FillUp fu : fillups ) {
+			if ( best > fu.getMPG() ) {
+				best = fu.getMPG();
+			}
+		}
+		return round(best);
+	}
+
+	public double getBestPPGRecord(List<FillUp> fillups)
+	{
+		double best = 0.0;
+		for ( FillUp fu : fillups ) {
+			if ( best < fu.getPrice() ) {
+				best = fu.getPrice();
+			}
+		}
+		return round(best);
+	}
+
+	public double getWorstPPGRecord(List<FillUp> fillups)
+	{
+		double best = Double.MAX_VALUE;
+		for ( FillUp fu : fillups ) {
+			if ( best > fu.getPrice() ) {
+				best = fu.getPrice();
+			}
+		}
+		return round(best);
+	}
+
+	public double getBestMFCRecord(List<FillUp> fillups)
+	{
+		Double[] values = new Double[12];
+		Arrays.fill(values, 0.0);
+
+		for ( FillUp fu : fillups ) {
+			values[fu.getDate().getMonth()] += fu.getPrice() * fu.getGas();
+		}
+
+		double max = values[0];
+
+		for ( int i = 1; i < values.length; i++ ) {
+			if ( values[i] > max ) {
+				max = values[i];
+			}
+		}
+
+		return round(max);
+	}
+
+	public double getWorstMFCRecord(List<FillUp> fillups)
+	{
+		Double[] values = new Double[12];
+		Arrays.fill(values, 0.0);
+
+		for ( FillUp fu : fillups ) {
+			values[fu.getDate().getMonth()] += fu.getPrice() * fu.getGas();
+		}
+
+		double max = values[0];
+
+		for ( int i = 1; i < values.length; i++ ) {
+			if ( values[i] < max ) {
+				max = values[i];
+			}
+		}
+
+		return max;
+	}
+
+	public double getBestCPMRecord(List<FillUp> fillups)
+	{
+		double best = 0.0;
+		for ( FillUp fu : fillups ) {
+			if ( best < ( fu.getMPG() / fu.getPrice() ) ) {
+				best = ( fu.getMPG() / fu.getPrice() );
+			}
+		}
+		return round(best);
+	}
+
+	public double getWorstCPMRecord(List<FillUp> fillups)
+	{
+		double best = Double.MAX_VALUE;
+		for ( FillUp fu : fillups ) {
+			if ( best > ( fu.getMPG() / fu.getPrice() ) ) {
+				best = ( fu.getMPG() / fu.getPrice() );
+			}
+		}
+		return round(best);
+	}
+
+	public double getBestMMRecord(List<FillUp> fillups)
+	{
+		Double[] values = new Double[12];
+		Arrays.fill(values, 0.0);
+
+		for ( FillUp fu : fillups ) {
+			values[fu.getDate().getMonth()] += fu.getDistance();
+		}
+
+		double max = values[0];
+
+		for ( int i = 1; i < values.length; i++ ) {
+			if ( values[i] > max ) {
+				max = values[i];
+			}
+		}
+
+		return round(max);
+	}
+
+	public double getWorstMMRecord(List<FillUp> fillups)
+	{
+		Double[] values = new Double[12];
+		Arrays.fill(values, 0.0);
+
+		for ( FillUp fu : fillups ) {
+			values[fu.getDate().getMonth()] += fu.getDistance();
+		}
+
+		double max = values[0];
+
+		for ( int i = 1; i < values.length; i++ ) {
+			if ( values[i] < max ) {
+				max = values[i];
+			}
+		}
+
+		return round(max);
+	}
+
+	private double round(double x) 
+	{
+		return ((int)((x * 100) / 100));
+	}
 }
